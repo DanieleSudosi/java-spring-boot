0b9912aa863c1d281d5b61c746b129c71a9b5e4e
DanieleSudosi <96229570+DanieleSudosi@users.noreply.github.com>
2022-03-21 11:26:41 +0100

    Delete concessionaria-auto directory

--- concessionaria-auto/src/main/java/com/sudo/dal/AutoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.sudo.dal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sudo.entities.Auto;
-
-public interface AutoDAO extends JpaRepository<Auto, Integer> {
-
-}
