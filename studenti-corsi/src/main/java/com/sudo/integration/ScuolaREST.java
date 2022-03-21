df85b7b706dafed701d78bd18c01a1761dc6a118
DanieleSudosi <96229570+DanieleSudosi@users.noreply.github.com>
2022-03-21 11:28:35 +0100

    Add files via upload

---
+++ studenti-corsi/src/main/java/com/sudo/integration/ScuolaREST.java
@@ -0,0 +1,46 @@
+package com.sudo.integration;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sudo.entities.Esame;
+import com.sudo.entities.Studente;
+import com.sudo.entities.StudentiEsami;
+import com.sudo.services.ScuolaService;
+
+@RestController
+@RequestMapping("api")
+public class ScuolaREST {
+
+	@Autowired
+	private ScuolaService service;
+	
+	@GetMapping("studenti")
+	public List<Studente> getStudenti() {
+		return service.getStudenti();
+	}
+	
+	@GetMapping("esami")
+	public List<Esame> getEsami() {
+		return service.getEsami();
+	}
+	@GetMapping("studentiesami")
+	public List<StudentiEsami> getStudentiEsami() {
+		return service.getStudentiEsami();
+	}
+	
+	@GetMapping("esamibystudente/{id}")
+	public List<Esame> getEsamiByStudente(@PathVariable int id){
+		return service.getEsameByStudente(service.getStudenteById(id));
+	}
+	@GetMapping("studentebyesame/{id}")
+	public List<Studente> getStudenteByEsame(@PathVariable int id){
+		return service.getStudenteByEsame(service.getEsameById(id));
+	}
+	
+}
