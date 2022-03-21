900b82518ee3df3d652af0f99b31ec865161efc8
DanieleSudosi <96229570+DanieleSudosi@users.noreply.github.com>
2022-03-21 11:27:02 +0100

    Delete quiz-app directory

--- quiz-app/src/main/java/com/sudo/dal/QuestionDAO.java
+++
@@ -1,9 +0,0 @@
-package com.sudo.dal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sudo.entities.Question;
-
-public interface QuestionDAO extends JpaRepository<Question, Integer> {
-
-}
--- quiz-app/src/main/java/com/sudo/dal/ResultDAO.java
+++
@@ -1,9 +0,0 @@
-package com.sudo.dal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sudo.entities.Result;
-
-public interface ResultDAO extends JpaRepository<Result, Integer>{
-
-}
